2fd3c88a41eed8ddffd3dc4bb0d277782eaab066
Branchey <70140890+CZboop@users.noreply.github.com>
2022-07-18 20:37:53 +0100

    refactored recommender into a class

--- server_side/als_recommender.py
+++ server_side/als_recommender.py
@@ -1,9 +1,7 @@
 import pandas as pd
 import numpy as np
 import random
-from scipy.sparse import csr_matrix
 import math
-from sklearn.neighbors import NearestNeighbors
 from fuzzywuzzy import fuzz
 import time
 
@@ -13,30 +11,35 @@ from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.recommendation import ALS
 from pyspark.ml.tuning import ParamGridBuilder
 
-spark_session = SparkSession.builder.appName("Music Recommender").getOrCreate()
-sp_con = spark_session.sparkContext
+class Recommender:
+    def __init__(self):
+        self.spark_session = SparkSession.builder.appName("Music Recommender").getOrCreate()
+        self.sp_con = self.spark_session.sparkContext
+        self._load_data()
+        self._make_model()
 
-artists = spark_session.read.csv('./data/lastfm_artist_list.csv', inferSchema=True, header=True)
-# switch the user_listens one with 5-10 rating processed?
-user_listens = spark_session.read.csv('./data/lastfm_user_scrobbles.csv', inferSchema=True, header=True)
-artists.printSchema()
-user_listens.printSchema()
+    def _load_data(self):
+        self.artists = self.spark_session.read.csv('./data/lastfm_artist_list.csv', inferSchema=True, header=True)
+        self.user_listens = self.spark_session.read.csv('./data/lastfm_user_scrobbles.csv', inferSchema=True, header=True)
 
-training_df, test_df = user_listens.randomSplit([.8, .2])
+    def _make_model(self):
+        self.training_df, self.test_df = self.user_listens.randomSplit([.8, .2])
 
-# parameters for the model
-iterations = 12
-regularisation = 0.1
-rank = 8
-errors = []
-err = 0
+        # parameters for the model
+        iterations = 12
+        regularisation = 0.1
+        rank = 8
+        errors = []
+        err = 0
 
-als_model = ALS(maxIter = iterations, rank = rank, regParam = regularisation,
-userCol='user_id', itemCol='artist_id', ratingCol='scrobbles', coldStartStrategy="drop")
+        self.als_model = ALS(maxIter = iterations, rank = rank, regParam = regularisation,
+        userCol='user_id', itemCol='artist_id', ratingCol='scrobbles', coldStartStrategy="drop")
 
-# training it
-model = als_model.fit(training_df)
-model.recommendForAllUsers(5)
+        self.model = self.als_model.fit(self.training_df)
+
+    def recommend_all(self, n_artists = 5):
+        self.model.recommendForAllUsers(n_artists)
 
 if __name__=="__main__":
-    print("run")
+    recommender = Recommender()
+    recommender.recommend_all()

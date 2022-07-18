d2cffe26047c72e17459202257771a70b1da92e6
Branchey <70140890+CZboop@users.noreply.github.com>
2022-07-18 20:19:42 +0100

    added data files and tested they load in to the spark session

--- server_side/als_recommender.py
+++ server_side/als_recommender.py
@@ -7,6 +7,20 @@ from sklearn.neighbors import NearestNeighbors
 from fuzzywuzzy import fuzz
 import time
 
+from pyspark.sql import SparkSession, Row
+from pyspark.sql.functions import col, lower
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml.recommendation import ALS
+from pyspark.ml.tuning import ParamGridBuilder
+
+spark_session = SparkSession.builder.appName("Music Recommender").getOrCreate()
+sp_con = spark_session.sparkContext
+
+artists = spark_session.read.csv('./data/lastfm_artist_list.csv', inferSchema=True, header=True)
+# switch the user_listens one with 5-10 rating processed?
+user_listens = spark_session.read.csv('./data/lastfm_user_scrobbles.csv', inferSchema=True, header=True)
+artists.printSchema()
+user_listens.printSchema()
 
 if __name__=="__main__":
     print("run")

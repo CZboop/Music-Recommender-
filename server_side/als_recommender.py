c053fca5b14ef0b807112a9b218026e93c54d78f
Branchey <70140890+CZboop@users.noreply.github.com>
2022-07-18 20:13:02 +0100

    added requirements file and created file for recommender logic

---
+++ server_side/als_recommender.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import random
+from scipy.sparse import csr_matrix
+import math
+from sklearn.neighbors import NearestNeighbors
+from fuzzywuzzy import fuzz
+import time
+
+
+if __name__=="__main__":
+    print("run")

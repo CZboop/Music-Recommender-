1575f15eafba21bd14462331deb1bce3a0146659
Branchey <70140890+CZboop@users.noreply.github.com>
2022-09-06 21:38:31 +0100

    updated requirements

--- server_side/add_artist_urls.py
+++ server_side/add_artist_urls.py
@@ -3,6 +3,8 @@ from api_main import get_db_connection
 import http.client
 import requests
 import time
+import httplib2
+from bs4 import BeautifulSoup, SoupStrainer
 
 # adding a new column to store lastfm link, can adjust and add to initial table
 def add_url_column():
@@ -26,20 +28,27 @@ def get_artist_names():
 
 # getting lastfm urls
 def get_artist_urls(artist_names):
+    http = httplib2.Http()
     #checking if url fits standard with status code check
     invalid_artists = []
     for index, artist in enumerate(artist_names):
         # limiting requests to 50 per minute
-        if index % 50 == 0:
+        if index > 0 and index % 50 == 0:
             time.sleep(60)
         url = f"https://www.last.fm/music/{'+'.join(artist.split())}"
         try:
             req = requests.head(url)
+            status, response = http.request(url)
+            soup = BeautifulSoup(response, 'html.parser')
             # TODO: deal with invalid urls - mostly 301 redirects, some 404s for really odd names need workarounds
-            if req.status_code != 200:
+            if status != 200:
+                if status == 301:
+                    print('301')
+                    print(soup.findall('td',{'class': 'chartlist-name'}))
                 print(artist)
                 print(url)
-                print(req.status_code)
+                print(status)
+                # print(req.status_code)
                 invalid_artists.append(artist)
         except requests.ConnectionError:
             print("connection error")

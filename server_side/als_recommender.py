d9620205ab19edcd61bd9ce6800a3884a65896ce
Branchey <70140890+CZboop@users.noreply.github.com>
2022-09-15 20:28:35 +0100

    edited recommender object to load in and use trained model rather than making and fitting again

--- server_side/als_recommender.py
+++ server_side/als_recommender.py
@@ -7,12 +7,14 @@ import time
 from sqlalchemy import create_engine
 import psycopg2
 import os
+import sys
 
 from pyspark.sql import SparkSession, Row
 from pyspark.sql.functions import col, lower
 from pyspark.ml.evaluation import RegressionEvaluator
-from pyspark.ml.recommendation import ALS
+from pyspark.ml.recommendation import ALS, ALSModel
 from pyspark.ml.tuning import ParamGridBuilder
+from pyspark.ml.pipeline import PipelineModel
 
 class Recommender:
     def __init__(self, iterations = 12, regularisation = 0.1, rank = 8):
@@ -23,7 +25,6 @@ class Recommender:
         self._map_artist_id()
 
     def _load_data(self):
-
         conn = psycopg2.connect(host='localhost',
                                 database='recommend',
                                 user=os.environ['DB_USERNAME'],
@@ -33,21 +34,12 @@ class Recommender:
         listen_data = cur.fetchall()
         cur.close()
         conn.close()
-        print('=========making df============')
+
         self.user_listens = self.spark_session.createDataFrame(listen_data)
 
     # may adjust parameters/make dynamic but these seem to be decent default
     def _make_model(self, iterations = 12, regularisation = 0.1, rank = 8):
-        self.training_df, self.test_df = self.user_listens.randomSplit([.8, .2])
-
-        errors = []
-        err = 0
-        print('==========making model============')
-        self.als_model = ALS(maxIter = iterations, rank = rank, regParam = regularisation,
-        userCol='_2', itemCol='_3', ratingCol='_4', coldStartStrategy="drop")
-        print('=========fitting model============')
-        self.model = self.als_model.fit(self.training_df)
-        print('============model complete========')
+        self.model = self.load_model()
 
 
     def _map_artist_id(self):
@@ -80,3 +72,7 @@ class Recommender:
         subset = self.user_listens.filter(self.user_listens._3 == user_id)
         # subset.select("user_id").limit(1).show()
         return subset
+
+    def load_model(self, path='trained_model'):
+        trained_model = ALSModel.load(path)
+        return trained_model

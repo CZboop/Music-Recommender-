b12d9cf3d6bc56dcf45f3483359240484a2495e5
Branchey <70140890+CZboop@users.noreply.github.com>
2022-09-17 14:02:02 +0100

    edited model making script to tune hyperparameters before saving best version of the model

--- server_side/make_model.py
+++ server_side/make_model.py
@@ -9,10 +9,10 @@ from pyspark.sql import SparkSession, Row
 from pyspark.sql.functions import col, lower
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.recommendation import ALS
-from pyspark.ml.tuning import ParamGridBuilder
+from pyspark.ml.tuning import ParamGridBuilder, CrossValidator
 
 class Model:
-    def __init__(self, iterations = 12, regularisation = 0.1, rank = 8):
+    def __init__(self):
         # needs hadoop path as env variable can set here or in terminal etc.
         # also requires .dll in same bin folder, .dll in windows system32 and may need more setup if setting up on a new machine
         os.environ['HADOOP_HOME'] = "C:/winutils"
@@ -21,7 +21,13 @@ class Model:
         self.spark_session = SparkSession.builder.master("local").appName("Music Recommender").getOrCreate()
         self.sp_con = self.spark_session.sparkContext
         self._load_data()
-        self._make_and_train_model(iterations, regularisation, rank)
+        self._make_base_model()
+
+        params = self._tune()
+
+        rank, max_iter, reg_param = params[0], params[1], params[2]
+
+        self._make_and_train_model(max_iter, reg_param, rank)
 
     def _load_data(self):
         conn = psycopg2.connect(host='localhost',
@@ -35,16 +41,42 @@ class Model:
         conn.close()
         self.user_listens = self.spark_session.createDataFrame(listen_data).repartition(200)
 
-    def _make_and_train_model(self, iterations = 12, regularisation = 0.1, rank = 8):
-        self.training_df, self.test_df = self.user_listens.randomSplit([.8, .2])
+    # setting up a model for tuning against
+    def _make_base_model(self):
+        self.training_df, self.test_df = self.user_listens.randomSplit([.8, .2], seed = 42)
 
+        errors = []
+        err = 0
+
+        self.als_model = ALS(userCol='_2', itemCol='_3', ratingCol='_4', coldStartStrategy="drop")
+
+    def _make_and_train_model(self, iterations = 12, regularisation = 0.1, rank = 8):
         errors = []
         err = 0
 
         self.als_model = ALS(maxIter = iterations, rank = rank, regParam = regularisation,
         userCol='_2', itemCol='_3', ratingCol='_4', coldStartStrategy="drop")
 
-        self.model = self.als_model.fit(self.training_df)
+        #final model using all data, rather than train test split
+        self.model = self.als_model.fit(self.user_listens)
+
+    # tuning and evaluating model with different hyperparameters to get the best/ a better version of it
+    #
+    def _tune(self):
+        param_grid = ParamGridBuilder()\
+            .addGrid(self.als_model.rank, [i for i in range(5, 16)])\
+            .addGrid(self.als_model.maxIter, [i for i in range(6, 24, 4)])\
+            .addGrid(self.als_model.regParam, [0.05, 0.1, 0.15, 0.3, 0.4]).build()
+        eval = RegressionEvaluator(metricName = "rmse", labelCol = '_4', predictionCol = 'prediction')
+        cross_validator = CrossValidator(estimator = self.als_model, estimatorParamMaps = param_grid, evaluator = eval, numFolds = 5)
+        print(cross_validator)
+        models = cross_validator.fit(self.training_df)
+        best_model = models.bestModel
+        print(best_model)
+        # extracting hyperparameters from the best model
+        best_rank, best_maxIter, best_regParam = best_model.getRank(), best_model.getMaxIter(), best_model.getRegParam()
+
+        return [best_rank, best_maxIter, best_regParam]
 
     def save_model(self, dir_name = 'trained_model'):
         # saving trained model to the folder specified in args
@@ -52,4 +84,5 @@ class Model:
 
 if __name__=="__main__":
     model = Model()
+    # model._tune()
     model.save_model()

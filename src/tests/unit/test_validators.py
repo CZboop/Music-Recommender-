99ea8ae206e3d5f6f9ea0a000cbba871cef70325
Branchey <70140890+CZboop@users.noreply.github.com>
2022-11-09 12:36:23 +0000

    added some tests for password validator and adjusted imports for db tests

--- src/tests/unit/test_validators.py
+++ src/tests/unit/test_validators.py
@@ -5,15 +5,25 @@ class TestValidators(unittest.TestCase):
 
     ### TEST VALIDATORS
 
-    def test_password_validator_length(self):
-        actual = None
-        expected = None
-        self.assertEqual(expected, actual)
+    def test_password_validator_valid_returns_true(self):
+        validator = PasswordValidator('T3stP@ssword123')
+        actual = validator.is_valid()
+        self.assertTrue(actual)
 
-    def test_password_validator_uppercase(self):
-        actual = None
-        expected = None
-        self.assertEqual(expected, actual)
+    def test_password_validator_low_length_returns_false(self):
+        validator = PasswordValidator('P@s1')
+        actual = validator.is_valid()
+        self.assertFalse(actual)
+
+    def test_password_validator_no_uppercase_returns_false(self):
+        validator = PasswordValidator('p@ssword12345!')
+        actual = validator.is_valid()
+        self.assertFalse(actual)
+    
+    def test_password_validator_no_lowercase_returns_false(self):
+        validator = PasswordValidator('P@SSWORD12345!')
+        actual = validator.is_valid()
+        self.assertFalse(actual)
 
 if __name__=="__main__":
     unittest.main()
